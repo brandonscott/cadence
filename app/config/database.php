11d03a0297654e2ccf413afaa7a07f7ff9757b65
Brandon <brandon.scott03@gmail.com>
2014-05-05 14:59:36 +0000

    Updated DB details

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => '127.0.0.1',
 			'database'  => 'cadence',
-			'username'  => 'root',
-			'password'  => 'U!ystTLUe%$sdz3',
+			'username'  => 'cadence',
+			'password'  => 'Cadenc3!',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
