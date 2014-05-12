f68b920f1a3d79f2e887043b7c51e8d70221e123
Brandon <brandon.scott03@gmail.com>
2014-05-12 09:07:43 +0100

    Tidies up the Server model and adds scopes for finding specific models.

--- app/models/Server.php
+++ app/models/Server.php
@@ -1,28 +1,43 @@
 <?php
+    class Server extends Eloquent {
 
-class Server extends Eloquent {
+    	/**
+    	 * The attributes excluded from the model's JSON form.
+    	 *
+    	 * @var array
+    	 */
+    	protected $fillable = array('servergroup_id', 'name', 'available_disk', 'available_ram', 'cpu_speed', 'os_name', 'os_version');
+    	protected $guarded = array('id', 'guid');
+    	public $timestamps = false;
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $fillable = array('servergroup_id', 'description', 'available_disk', 'available_ram', 'cpu_speed', 'os_name', 'os_version');
-	protected $guarded = array('id', 'guid');
-	public $timestamps = false;
+    	/**
+        * Store a newly created resource in storage.
+        *
+        * @return Response
+        */
+        public function store()
+        {
+            //
+        }
 
-	/**
-    * Store a newly created resource in storage.
-    *
-    * @return Response
-    */
-    public function store()
-    {
-        //
+        public function pulse()
+        {
+        	return $this->hasMany('Pulse', 'server_id', 'id');
+        }
+
+        public function scopeOfUnassigned($query, $status)
+        {
+            return $query->where('servergroup_id', '=', $status);
+        }
+
+        public function scopeofAssigned($query, $status)
+        {
+            return $query->where('servergroup_id', '>', $status);
+        }
+
+        public function scopeOfStatus($query, $status)
+        {
+            return $query->where('online', '=', $status);
+        }
     }
-
-    public function pulse()
-    {
-    	return $this->hasMany('Pulse', 'server_id', 'id');
-    }
-}
+?>
