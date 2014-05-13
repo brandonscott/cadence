dc9fce727bdc2fb232eb0048999ed6ec4271ca1c
Brandon <brandon.scott03@gmail.com>
2014-05-13 09:55:49 +0100

    Updated routes

--- app/routes.php
+++ app/routes.php
@@ -13,9 +13,12 @@
 
 //User routes
 Route::get('/users', 'UserController@getAll');
+Route::get('/users/{id}', 'UserController@getUser');
 Route::get('/users/{id}/servergroups/default', 'UserController@getDefaultServerGroup');
 Route::post('/users', 'UserController@store');
 Route::delete('/users/{id}', 'UserController@deleteUser');
+Route::get('/users/{id}/subscriptions', 'UserController@getSubscriptions');
+Route::put('/subscriptions/{subid}', 'UserController@updateSubscription');
 
 //Auth route
 Route::get('/auth', 'AuthenticationController@auth');
@@ -29,9 +32,10 @@ Route::group(array('before' => 'superadmin.auth'), function() {
 	//Server routes
 	Route::get('/servers/unassigned', 'ServerController@getUnassignedServers');
 	Route::get('/servers/assigned', 'ServerController@getAssignedServers');
+	Route::put('/servers/{id}/servergroup', 'ServerController@updateServerGroup');
 	Route::get('/servers/status/{status}', 'ServerController@getServersForStatus');
 	Route::get('/servers/{id}', 'ServerController@getServer');
-	Route::put('/servers/{id}', 'ServerController@updateServerDetails');
+	Route::put('/servers/{guid}', 'ServerController@updateServerDetails');
 	Route::delete('/servers/{id}', 'ServerController@deleteServer');
 	Route::get('/servers', 'ServerController@getAll');
 	Route::post('/servers', 'ServerController@store');
@@ -43,6 +47,7 @@ Route::group(array('before' => 'superadmin.auth'), function() {
 
 	//Server Group routes
 	Route::get('/servergroups', 'ServerGroupController@getAll');
+	Route::get('/servergroups/{id}', 'ServerGroupController@getServerGroup');
 	Route::post('/servergroups', 'ServerGroupController@store');
 	Route::delete('/servergroups/{id}', 'ServerGroupController@deleteServerGroup');
 	Route::get('/servergroups/{id}/servers', 'ServerGroupController@getServers');
